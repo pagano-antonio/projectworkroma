e69177aae9bee3ec808aeb4346080c9f34d3d6c8
pagano.antonio <iatix1@gmail.com>
2024-02-29 12:45:11 +0100

    first commit

---
+++ ProjectWorkRomaBE/src/main/java/com/ProjectWorkRomaBeApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectWorkRomaBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectWorkRomaBeApplication.class, args);
+	}
+
+}
