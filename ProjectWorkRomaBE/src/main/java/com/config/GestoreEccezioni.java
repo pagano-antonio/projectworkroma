1846f0f6baf4e1b620ecb006955703f8b0abde53
antomar93 <antomar.93@gmail.com>
2024-03-04 18:15:05 +0100

    DB aggiornato

---
+++ ProjectWorkRomaBE/src/main/java/com/config/GestoreEccezioni.java
@@ -0,0 +1,17 @@
+package com.config;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GestoreEccezioni {
+	
+    @ExceptionHandler(value= Exception.class)
+	public String handleGlobalException(Exception e, Model model) {
+		e.printStackTrace();
+    	String msg =e.getMessage();
+		model.addAttribute("messaggioErrore",e);
+    	return "systemError";
+	}
+}
