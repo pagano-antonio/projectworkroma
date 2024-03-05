4b35b7bf67b7ddd25f9b8a768359f68f2d81205a
CatarinaMartins01 <just.gonky.design@gmail.com>
2024-03-05 15:39:55 +0100

    helloo

--- ProjectWorkRomaBE/src/main/java/com/dao/JobInterviewRepository.java
+++ ProjectWorkRomaBE/src/main/java/com/dao/JobInterviewRepository.java
@@ -2,9 +2,10 @@ package com.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import com.model.Candidate;
+
+import com.model.JobInterview;
 
 @Repository
-public interface CandidateRepository extends JpaRepository<Candidate, Integer>{
+public interface JobInterviewRepository extends JpaRepository<JobInterview, Integer> {
 
 }
--- ProjectWorkRomaBE/src/main/java/com/dao/JobOfferSkillRepository.java
+++ ProjectWorkRomaBE/src/main/java/com/dao/JobOfferSkillRepository.java
@@ -2,9 +2,10 @@ package com.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import com.model.Candidate;
+
+import com.model.JobOfferSkill;
 
 @Repository
-public interface CandidateRepository extends JpaRepository<Candidate, Integer>{
+public interface JobOfferSkillRepository extends JpaRepository<JobOfferSkill, Integer>{
 
 }
