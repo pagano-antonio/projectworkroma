60b091f45eac196ddb5adb9203e88e38ed4ba822
AriannaFelli <ariannafelli2000@icloud.com>
2024-03-05 11:09:12 +0100

    ciao

--- ProjectWorkRomaBE/src/main/java/com/ServletInitializer.java
+++ ProjectWorkRomaBE/src/main/java/com/ServletInitializer.java
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(GestionaleSpringBootApplication.class);
+		return application.sources(ProjectWorkRomaBeApplication.class);
 	}
 
 }
