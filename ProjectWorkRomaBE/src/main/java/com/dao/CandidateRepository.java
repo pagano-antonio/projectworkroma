cc02f2539577a642757b57aa87192294475782c2
AriannaFelli <ariannafelli2000@icloud.com>
2024-03-05 15:39:28 +0100

    super grrr

--- ProjectWorkRomaBE/src/main/java/com/dao/CandidateRepository.java
+++ ProjectWorkRomaBE/src/main/java/com/dao/CandidateRepository.java
@@ -1,23 +1,13 @@
 package com.dao;
 
-<<<<<<< HEAD
+
 	import org.springframework.data.jpa.repository.JpaRepository;
 	import org.springframework.stereotype.Repository;
 
 	import com.model.Candidate;
 
-	@Repository
-	public interface CandidateRepository extends JpaRepository<Candidate, Integer>{
-
-	}
-=======
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import com.model.Candidate;
-
 @Repository
 public interface CandidateRepository extends JpaRepository<Candidate, Integer>{
 
-}
->>>>>>> 79068d7c1fff5fa1cd64aba760ab4eb15690a638
+} 
+
---
+++ ProjectWorkRomaBE/src/main/java/com/dao/EmployeeTypeRepository.java
@@ -0,0 +1,10 @@
+package com.dao;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.model.EmployeeType;
+
+@Repository
+public interface EmployeeTypeRepository extends JpaRepository <EmployeeType, Integer> {
+
+} 
+
