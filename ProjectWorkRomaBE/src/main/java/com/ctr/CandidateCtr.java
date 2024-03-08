e7403e33cd50ac57abf815de0873759548b64984
AriannaFelli <161700992+AriannaFelli@users.noreply.github.com>
2024-03-08 12:44:59 +0100

    Aggiunta metodi findby surname/idCandidate e city

--- ProjectWorkRomaBE/src/main/java/com/ctr/CandidateCtr.java
+++ ProjectWorkRomaBE/src/main/java/com/ctr/CandidateCtr.java
@@ -1,4 +1,6 @@
 package com.ctr;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.stereotype.Controller;
@@ -6,9 +8,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.dao.CandidateRepository;
 import com.model.Candidate;
+
 
 
 @Controller  
@@ -75,10 +79,43 @@ return "errore";
 
 ////////////////////////////////////// FIND BY ID //////////////////////////////////
 
+@GetMapping("/candidate/preFindByIdCandidate") 
+public String preFindByIdCandidate(Model model) {
+	return "findByIdCandidate";
+}
+
+@GetMapping("/candidate/findByIdCandidate") 
+public String findByIdCandidate(Model model, Integer idCandidate) {
+	List<Candidate> candidate = candidateRep.findByIdCandidate(idCandidate);
+	model.addAttribute("idCandidate", candidate);
+	return "resultsFindByIdCandidate";   
+}
 ////////////////////////////////////// FIND BY SURNAME /////////////////////////////
 
+@GetMapping("/candidate/preFindBySurname") 
+public String preFindBySurname(Model model) {
+	return "findBySurname";
+}
+
+@GetMapping("/candidate/findBySurname") 
+public String findBySurname(Model model, String surname) {
+	List<Candidate> candidate = candidateRep.findBySurname(surname);
+	model.addAttribute("surnameCandidate", candidate);
+	return "resultsFindBySurname";  
+}
 ////////////////////////////////////// FIND BY CITY ////////////////////////////////
 
+@GetMapping("/candidate/preFindByCity") 
+public String preFindByCity(Model model) {
+	return "findByCity";
+}
+
+@GetMapping("/candidate/findByCity") 
+public String findByCity(Model model, String city) {
+	List<Candidate> candidate = candidateRep.findByCity(city);
+	model.addAttribute("city", candidate);
+	return "resultsFindByCity"; 
+}
 ////////////////////////////////////// FIND BY PHONE NUMBER ////////////////////////
 
 ////////////////////////////////////// FIND BY SKILL ///////////////////////////////
@@ -89,6 +126,6 @@ return "errore";
 
 ////////////////////////////////////// FIND BY ID STATE JOB INTERVIEW per Outcome//////////////////
 
-////////////////////////////////////// FIND BY ANNI DI ESPERIENZA //////////////////
+
 
 }
--- ProjectWorkRomaBE/src/main/java/com/dao/CandidateRepository.java
+++ ProjectWorkRomaBE/src/main/java/com/dao/CandidateRepository.java
@@ -1,5 +1,7 @@
 package com.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,6 +9,12 @@ import com.model.Candidate;
 
 @Repository
 public interface CandidateRepository extends JpaRepository<Candidate, Integer>{
+
+	List<Candidate> findByIdCandidate(Integer idCandidate);
+
+	List<Candidate> findBySurname(String surname);
+
+	List<Candidate> findByCity(String city);
 
 } 
 
