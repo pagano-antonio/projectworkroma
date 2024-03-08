bfe2f366162e50c4dc49e2e8ffbfdaff4a31f854
AriannaFelli <161700992+AriannaFelli@users.noreply.github.com>
2024-03-08 15:48:09 +0100

    job

--- ProjectWorkRomaBE/src/main/java/com/dao/CandidateRepository.java
+++ ProjectWorkRomaBE/src/main/java/com/dao/CandidateRepository.java
@@ -4,9 +4,12 @@ import java.math.BigInteger;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+
 import com.model.Candidate;
+import com.model.JobInterview;
 import com.model.Skill;
 
 @Repository
@@ -24,8 +27,10 @@ public interface CandidateRepository extends JpaRepository<Candidate, Integer>{
 
 	List<Candidate> findByPhone(BigInteger phone);
 
-	List<Candidate> findByYearsOfExperienceCandidate(String yearsOfExperienceCandidate); 
-
-} 
+//	List<WorkExperience> findByWorkExperiences_WorkExperience_StartDateAfterAndEndDateBefore(Date startDate, Date endDate );
+	
+	@Query(value ="SELECT c FROM Candidate c WHERE c.jobInterviews.outcome=:outcome")
+	List<JobInterview> findByOutcome(Integer outcome); 
+}  
 
 
