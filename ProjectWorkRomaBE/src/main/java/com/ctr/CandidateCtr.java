9741f8d64ed57bd019f2fa1ed11a32369761916b
CatarinaMartins01 <just.gonky.design@gmail.com>
2024-03-08 14:33:48 +0100

    Update CandidateCtr.java

--- ProjectWorkRomaBE/src/main/java/com/ctr/CandidateCtr.java
+++ ProjectWorkRomaBE/src/main/java/com/ctr/CandidateCtr.java
@@ -9,12 +9,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.dao.CandidateRepository;
 import com.model.Candidate;
-import com.model.CompanyClient;
-import com.model.Education;
 import com.model.Skill;
 
 
