5b958e131e601da13e11042760f29809e05daa3f
anto 🌺 <161697608+antomar93@users.noreply.github.com>
2024-03-08 10:28:52 +0100

    prova connessione, con home vuota

--- ProjectWorkRomaBE/src/main/java/com/ctr/JobInterviewCtr.java
+++ ProjectWorkRomaBE/src/main/java/com/ctr/JobInterviewCtr.java
@@ -10,63 +10,63 @@ import org.springframework.web.bind.annotation.RequestParam;
 import com.dao.JobInterviewRepository;
 import com.model.JobInterview;
 
-@Controller  
+@Controller
 @RequestMapping
 public class JobInterviewCtr {
-	
+
 	@Autowired
 	private JobInterviewRepository jobInterviewRep;
-	
-//////////////////////////////////////ADD METHOD //////////////////////////////////////////////////////////
-	
-@GetMapping("/job/preAddJobInterview")    
-	public String preAddJobInterview(Model model) {
-	return "job/addJobInterview";
-}
 
-@PostMapping("/job/addJobInterview") 
-public String addJobInterview(Model model, JobInterview jobInter) {
-	jobInterviewRep.save(jobInter); 
-	return "success";
-}
+//////////////////////////////////////ADD METHOD //////////////////////////////////////////////////////////
+
+	@GetMapping("/job/preAddJobInterview")
+	public String preAddJobInterview(Model model) {
+		return "job/addJobInterview";
+	}
+
+	@PostMapping("/job/addJobInterview")
+	public String addJobInterview(Model model, JobInterview jobInter) {
+		jobInterviewRep.save(jobInter);
+		return "success";
+	}
 
 //////////////////////////////////////UPDATE METHOD //////////////////////////////////////////////////////////
 
-@GetMapping("/job/preUpdateByIdJobInterview")
-public String preUpdateByIdJobInterview() {
-return "job/updateIdJobInterview";
-}
+	@GetMapping("/job/preUpdateByIdJobInterview")
+	public String preUpdateByIdJobInterview() {
+		return "job/updateIdJobInterview";
+	}
 
-@GetMapping("/job/updateByIdJobInterview")
-public String updateByIdJobInterview(Model model, @RequestParam int idJobInterview) {
-JobInterview jobInterview = (JobInterview) jobInterviewRep.findById(idJobInterview).orElse(null);
-if (jobInterview != null) {
-model.addAttribute("idJobInterview", jobInterview);
-return "job/updateJobInterview";
-} else {
-String errorMessage = "ops!";
-model.addAttribute("errorMessage", errorMessage);
-return "errore";
-}
-}
+	@GetMapping("/job/updateByIdJobInterview")
+	public String updateByIdJobInterview(Model model, @RequestParam int idJobInterview) {
+		JobInterview jobInterview = (JobInterview) jobInterviewRep.findById(idJobInterview).orElse(null);
+		if (jobInterview != null) {
+			model.addAttribute("idJobInterview", jobInterview);
+			return "job/updateJobInterview";
+		} else {
+			String errorMessage = "ops!";
+			model.addAttribute("errorMessage", errorMessage);
+			return "errore";
+		}
+	}
 
 //////////////////////////////////////DELETE BY ID //////////////////////////////////////////////////////////
 
-@GetMapping("/job/preDeleteByIdJobInterview")
-public String preDeleteByIdJobInterview() {
-return "job/deleteByIdJobInterview";
-}
+	@GetMapping("/job/preDeleteByIdJobInterview")
+	public String preDeleteByIdJobInterview() {
+		return "job/deleteByIdJobInterview";
+	}
 
-@GetMapping("/job/deleteByIdJobInterview")
-public String deleteByIdJobInterview(Model model, Integer idJobInterview) {
-JobInterview jobInterview = (JobInterview) jobInterviewRep.findById(idJobInterview).orElse(null);
-if (jobInterview != null) {
-	jobInterviewRep.delete(jobInterview);
-return "success";
-} else {
-String errorMessage = "ops!";
-model.addAttribute("errorMessage", errorMessage);
-return "errore";
-}
-}
+	@GetMapping("/job/deleteByIdJobInterview")
+	public String deleteByIdJobInterview(Model model, Integer idJobInterview) {
+		JobInterview jobInterview = (JobInterview) jobInterviewRep.findById(idJobInterview).orElse(null);
+		if (jobInterview != null) {
+			jobInterviewRep.delete(jobInterview);
+			return "success";
+		} else {
+			String errorMessage = "ops!";
+			model.addAttribute("errorMessage", errorMessage);
+			return "errore";
+		}
+	}
 }
--- ProjectWorkRomaBE/src/main/java/com/ctr/JobOfferSkillCtr.java
+++ ProjectWorkRomaBE/src/main/java/com/ctr/JobOfferSkillCtr.java
@@ -11,61 +11,60 @@ import com.dao.JobOfferSkillRepository;
 import com.model.JobOfferSkill;
 
 public class JobOfferSkillCtr {
-	
+
 	@Autowired
 	private JobOfferSkillRepository jobOfferSkillRep;
-	
-//////////////////////////////////////ADD METHOD //////////////////////////////////////////////////////////
-	
-@GetMapping("/job/preAddJobOfferSkill")    
-	public String preAddJobOfferSkill(Model model) {
-	return "job/addJobOfferSkill";
-}
 
-@PostMapping("/job/addJobOfferSkill") 
-public String addJobOfferSkill(Model model, JobOfferSkill jobOfferSkill) {
-	jobOfferSkillRep.save(jobOfferSkill); 
-	return "success";
-}
+//////////////////////////////////////ADD METHOD //////////////////////////////////////////////////////////
+
+	@GetMapping("/job/preAddJobOfferSkill")
+	public String preAddJobOfferSkill(Model model) {
+		return "job/addJobOfferSkill";
+	}
+
+	@PostMapping("/job/addJobOfferSkill")
+	public String addJobOfferSkill(Model model, JobOfferSkill jobOfferSkill) {
+		jobOfferSkillRep.save(jobOfferSkill);
+		return "success";
+	}
 
 //////////////////////////////////////UPDATE METHOD //////////////////////////////////////////////////////////
 
-@GetMapping("/job/preUpdateByIdJobOfferSkill")
-public String preUpdateByIdIdJobOfferSkill() {
-return "job/updateIdJobOfferSkill";
-}
+	@GetMapping("/job/preUpdateByIdJobOfferSkill")
+	public String preUpdateByIdIdJobOfferSkill() {
+		return "job/updateIdJobOfferSkill";
+	}
 
-@GetMapping("/job/updateByIdJobOfferSkill")
-public String updateByIdJobOfferSkill(Model model, @RequestParam int idJobOfferSkill) {
-JobOfferSkill jobOfferSkill = (JobOfferSkill) jobOfferSkillRep.findById(idJobOfferSkill).orElse(null);
-if (jobOfferSkill != null) {
-model.addAttribute("idJobOfferSkill", jobOfferSkill);
-return "job/updateJobOfferSkill";
-} else {
-String errorMessage = "ops!";
-model.addAttribute("errorMessage", errorMessage);
-return "errore";
-}
-}
-
+	@GetMapping("/job/updateByIdJobOfferSkill")
+	public String updateByIdJobOfferSkill(Model model, @RequestParam int idJobOfferSkill) {
+		JobOfferSkill jobOfferSkill = (JobOfferSkill) jobOfferSkillRep.findById(idJobOfferSkill).orElse(null);
+		if (jobOfferSkill != null) {
+			model.addAttribute("idJobOfferSkill", jobOfferSkill);
+			return "job/updateJobOfferSkill";
+		} else {
+			String errorMessage = "ops!";
+			model.addAttribute("errorMessage", errorMessage);
+			return "errore";
+		}
+	}
 
 //////////////////////////////////////DELETE BY ID //////////////////////////////////////////////////////////
 
-@GetMapping("/job/preDeleteByIdJobOfferSkill")
-public String preDeleteByIdJobOfferSkill() {
-	return "job/deleteByIdJobOfferSkill";
-}
-
-@GetMapping("/job/deleteByIdJobOfferSkill")
-public String deleteByIdJobOfferSkill(Model model, Integer idJobOfferSkill) {
-	JobOfferSkill jobOfferSkill = (JobOfferSkill) jobOfferSkillRep.findById(idJobOfferSkill).orElse(null);
-	if (jobOfferSkill != null) {
-		jobOfferSkillRep.delete(jobOfferSkill);
-		return "success";
-	} else {
-		String errorMessage = "ops!";
-		model.addAttribute("errorMessage", errorMessage);
-		return "errore";
+	@GetMapping("/job/preDeleteByIdJobOfferSkill")
+	public String preDeleteByIdJobOfferSkill() {
+		return "job/deleteByIdJobOfferSkill";
 	}
-}
+
+	@GetMapping("/job/deleteByIdJobOfferSkill")
+	public String deleteByIdJobOfferSkill(Model model, Integer idJobOfferSkill) {
+		JobOfferSkill jobOfferSkill = (JobOfferSkill) jobOfferSkillRep.findById(idJobOfferSkill).orElse(null);
+		if (jobOfferSkill != null) {
+			jobOfferSkillRep.delete(jobOfferSkill);
+			return "success";
+		} else {
+			String errorMessage = "ops!";
+			model.addAttribute("errorMessage", errorMessage);
+			return "errore";
+		}
+	}
 }
