7bb05b7d811729238ab1ca1297c0852bd7653a0e
Triana Laurino <trii.laurino@gmail.com>
2024-03-08 14:57:46 +0100

    add/update methods Emp, company
    add methos ctr in employee, and update companyclient ctr

--- ProjectWorkRomaBE/src/main/java/com/ctr/EmployeeCtr.java
+++ ProjectWorkRomaBE/src/main/java/com/ctr/EmployeeCtr.java
@@ -7,9 +7,9 @@ package com.ctr;
 	import org.springframework.web.bind.annotation.GetMapping;
 	import org.springframework.web.bind.annotation.PostMapping;
 	import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-	
-    import com.dao.EmployeeRepository;
+import com.dao.EmployeeRepository;
     import com.model.Employee;
 
 
@@ -19,7 +19,7 @@ package com.ctr;
 
 	
 	@Autowired
-	private EmployeeRepository EmployeeRep;
+	private EmployeeRepository employeeRep;
 
 		
 ////////////////////////////////////// ADD METHOD //////////////////////////////////////////////////////////
@@ -31,11 +31,74 @@ package com.ctr;
 
 		@PostMapping("/company/addEmployee") 
 		public String addEmployee(Model model, Employee e) {
-			EmployeeRep.save(e); 
+			employeeRep.save(e); 
 			return "success";
 		}
 	
-////////////////////////////////// SEARCH BY ID ///////////////////////////////////////////////////////////
+//////////////////////////////////////DELETE BY ID   ////////////////////////////////////
+		
+		@GetMapping("/company/preDeleteByIdEmployee")
+		public String deleteByIdEmployee() {
+			return "company/deleteByInEmployee";
+		}
+		
+		@GetMapping("/company/deleteByIdEmployee")
+		public String deleteByIdEmployee(Model model, Integer idEmployee) {
+			Employee employee = (Employee) employeeRep.findById(idEmployee).orElse(null);
+			if(idEmployee != null) {
+				employeeRep.delete(employee);
+				return "sucess";
+			}else{
+				String errorMessage = "ops!";
+				model.addAttribute("errorMessage", errorMessage);
+				return "error";
+			}
+		}
+		
+//////////////////////////////////////UPDATE   /////////////////////////////////////////
 
+		@GetMapping("/company/preupdateByIdEmployee")
+		public String preupdateByIdEmployee() {
+			return "company/updateByIdEmployee";
+		}
+		
+		@GetMapping("/company/updateByIdEmployee")
+		public String updateByIdEmployee(Model model, @RequestParam int idEmployee) {
+			Employee employee = (Employee) employeeRep.findById(idEmployee).orElse(null);
+			if(employee != null) {
+				model.addAttribute("idEmployee", employee);
+				return "company/sucess";
+			}else {
+				String errorMessage = "ops!";
+				model.addAttribute("errorMessage", errorMessage);
+				return "errore";
+			}
+			
+		}
+		
+////////////////////////////////// FIND BY ID ///////////////////////////////////////////////////////////
+		
+		@GetMapping("/company/preFindhByIdEmployee")
+		public String preFindByIdEmployee() {
+			return "company/findByIdEmployee";
+		}
+		
+		@GetMapping("/company/findByIdEmployee")
+		public String findByIdEmployee(Model model, @RequestParam int idEmployee) {
+			Employee employee = (Employee) employeeRep.findById(idEmployee).orElse(null);
+			if(employee != null) {
+				model.addAttribute("idEmployee", employee);
+				return "company/resultsFindByIdEmployee";
+			}else {
+				String errorMessage = "ops!";
+				model.addAttribute("errorMessage", errorMessage);
+				return "error";
+			}
+			
+		}
+		
+		
+		
+		
 
 	}
