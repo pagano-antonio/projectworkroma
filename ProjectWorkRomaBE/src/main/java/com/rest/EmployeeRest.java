131198647561b063061c6b40e2e1f42e7b1e413b
anto 🌺 <161697608+antomar93@users.noreply.github.com>
2024-03-22 10:48:03 +0100

    rest

--- ProjectWorkRomaBE/src/main/java/com/rest/EmployeeRest.java
+++ ProjectWorkRomaBE/src/main/java/com/rest/EmployeeRest.java
@@ -1,8 +1,11 @@
 package com.rest;
 
 	import org.springframework.beans.factory.annotation.Autowired;
-	import org.springframework.web.bind.annotation.DeleteMapping;
-	import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 	import org.springframework.web.bind.annotation.PutMapping;
 	import org.springframework.web.bind.annotation.RequestBody;
 	import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,8 +15,10 @@ package com.rest;
    import com.model.Employee;
 
 
+
         @RestController
 		@RequestMapping("Employee")
+        @CrossOrigin
 		public class EmployeeRest { 
 			
 			@Autowired 
@@ -43,7 +48,10 @@ package com.rest;
 					 return "employeeDeleted"; 
 				 }
 
-		}
+///////////////////////////////////////FIND BY ID //////////////////////////////////////////////////////////		
 
-
-
+        @GetMapping("findById/{idEmployee}")
+         public Employee findByIdEmployee(@PathVariable("idEmployee") int idEmployee) {
+    		return EmployeeRep.findById(idEmployee).get();
+    	}
+        }
--- ProjectWorkRomaBE/src/main/webapp/views/addEmployee.jsp
+++ ProjectWorkRomaBE/src/main/webapp/views/addEmployee.jsp
@@ -1,6 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
 	pageEncoding="ISO-8859-1"%>
-		<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
 <%@page import="com.model.EmployeeType"%>
 <!DOCTYPE html>
 <html>
@@ -60,7 +60,15 @@
 						<option value="${listEmployeeType.idEmployeeType}">
 							${listEmployeeType.description}</option>
 					</c:forEach>
-				</select> <input type="submit" value="Register" class="buttonLogin" />
+				</select>
+
+				<p>
+					<label for="photo">Photo:</label>
+				</p>
+				<input class="inputField" type="file" id="photo" name="photo"
+					accept="image/*"> 
+					
+					<input type="submit" value="Register" class="buttonLogin" />
 
 			</form>
 			<a href="${pageContext.request.contextPath}/preLogin">
